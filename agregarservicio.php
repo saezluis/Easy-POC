4f08442463ea445472af78221edc10de16a4c1a2
iCharls <cflores@pm.cl>
2015-10-02 19:41:17 -0300

    hice otra line
    que dice weon!

--- agregarservicio.php
+++ agregarservicio.php
@@ -19,6 +19,8 @@
 
 	<!--hice mi cambio-->
 
+	<!--weon es pelua la cosa-->
+
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="js/typeahead.min.js"></script>
