c6672520b46af54e0e1acc69e56854700e420257
iCharls <cflores@pm.cl>
2015-10-02 19:59:46 -0300

    improbable
    cambios

--- agregarservicio.php
+++ agregarservicio.php
@@ -32,6 +32,8 @@
 	
 	<!-- algo nuevo -->
 
+	<!-- improbable-->
+
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="js/typeahead.min.js"></script>
