851938337c16bec1b45decaf21d02450fafb3768
iCharls <cflores@pm.cl>
2015-10-02 20:01:35 -0300

    te pase
    la pelota

--- agregarservicio.php
+++ agregarservicio.php
@@ -36,6 +36,8 @@
 
 	<!-- improbable-->
 
+	<!--te paso la pelota-->
+
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="js/typeahead.min.js"></script>
