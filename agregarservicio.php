de2da078b5196b5c624d0cf9244c8e9245c98ec5
iCharls <cflores@pm.cl>
2015-10-02 19:52:49 -0300

    prueba
    de comit 2

--- agregarservicio.php
+++ agregarservicio.php
@@ -27,8 +27,10 @@
 	5
 	-->
 
-
+	
 	<!--weon es pelua la cosa-->
+
+	<!--mike patton-->
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
