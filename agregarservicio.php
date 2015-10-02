abb190d3f508030a00a3bf40344b5174898b73cc
saezluis <lsaez@pm.cl>
2015-10-02 19:42:05 -0300

    nuevo cambio
    agreagado

--- agregarservicio.php
+++ agregarservicio.php
@@ -13,6 +13,9 @@
 	-->
 
 	<!--cambio aqui-->
+	<!--cambio aqui-->
+	
+	<!--cambio aqui-->
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
