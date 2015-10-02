d3e30c459c95b4d1a68e5e8e34792bdd9a88a56f
iCharls <cflores@pm.cl>
2015-10-02 19:38:31 -0300

    nueva line comentada
    dice "hce mi cambio"

--- agregarservicio.php
+++ agregarservicio.php
@@ -17,6 +17,8 @@
 	
 	<!--cambio aqui-->
 
+	<!--hice mi cambio-->
+
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="js/typeahead.min.js"></script>
