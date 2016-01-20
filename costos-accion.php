b078ab57e988395e1cda950ca4e8f210b7fded05
saezluis <lsaez@pm.cl>
2016-01-20 17:24:12 -0300

    trabajando en costos campana

--- costos-accion.php
+++ costos-accion.php
@@ -66,15 +66,71 @@ exit;
 			width: 190px;
 			height: 32px;
 		}
+		
+		.pptoReal {
+			width: 90px;
+		}
+		
+		.ccs_nroOC {
+			width: 90px;
+			border: none;
+			text-align: center;
+		}
+		
 	</style>
+	
+	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+	
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$("#nro_solicitud").keyup(function(){
+				//Get
+				var nro_so = $('#nro_solicitud').val();
+
+				//Set
+				$('#nroSolicitudBox').val(nro_so);				
+			});
+		})
+	</script>
 	
   </head>
   <body>
 	<?php
 		//header("Content-Type: text/html;charset=utf-8");
 		//$conexion=mysqli_connect("localhost","pmdigita_admin","Prodigy12","pmdigita_test") or die("Problemas con la conexión");	
-		$conexion=mysqli_connect("localhost","root","123","test") or die("Problemas con la conexión");		
+		include "config.php";
+		
+		//inicializacion de variales
+		$id_user="";
+		$nombre="";
+		$apellido="";
+		$nombre_completo="";
+		$jefe_autorizacion="";
+		$variable_increment = 0;
+		$variable_increment2 = 0;
+		$suma_PPTO = 0;
+		$suma_PPTO_format = 0;
+		$ppto_real="";
+		$ppto_real_suma="";
+		$ppto_real_suma_format=0;
+		$ppto_real_format=0;
+		
+		$conexion=mysqli_connect($host,$username,$password,$db_name) or die("Problemas con la conexión");
 		$acentos = $conexion->query("SET NAMES 'utf8'");
+		
+		$registrosCamapana=mysqli_query($conexion,"SELECT * FROM campana") or die("Problemas en el select:");	
+		
+		@$id_campana_get2 = @$_REQUEST['campana'];
+		
+		$registrosOC=mysqli_query($conexion,"SELECT * FROM ordenes WHERE campana = '$id_campana_get2'");
+		
+		$registrosOC2=mysqli_query($conexion,"SELECT * FROM ordenes WHERE campana = '$id_campana_get2'");
+		
+		$rows = mysqli_num_rows($registrosOC);
+		
+		echo "Esta devolviendo este nro de OC: ".$rows;
+		echo "<br>";
+		echo "<br>";
 		
 	?>
     <header class="grupo">
@@ -120,39 +176,77 @@ exit;
     </div>
     <div class="grupo no-padding">
       <div class="caja base-100">
-        <form id="search-form" method="POST" action="" class="info--cliente" name="myform" >
+        <form id="search-form" method="POST" action="costos-accion.php" class="info--cliente" name="myform" >
+		<!--
 		<button type="submit" value="Buscar" name="buscar" hidden=hidden></button>
+		-->
           <div class="caja base-20">
             <label>Acción Camapaña</label>
-			<select name="campana" class="" style="width: 190px;">
-              <option selected="" value="-1">Elija</option>
+			<select name="campana" class="" style="width: 190px;" onchange="this.form.submit()">
+				<?php
+					@$id_campana_get1 = @$_REQUEST['campana'];
+					echo "<option value=\"-1\" >Elija</option>";
+					while($reg=mysqli_fetch_array($registrosCamapana)){
+						$nombre_campana = $reg['nombre_campana'];
+						$id_campana = $reg['id_campana'];
+						
+						if(@$id_campana_get1==$id_campana){
+							echo "<option value=\"$id_campana\" selected=selected>$nombre_campana</option>";
+						}else{
+							echo "<option value=\"$id_campana\">$nombre_campana</option>";
+						}
+					}
+				?>
             </select>			
           </div>
 		  
           <div class="caja base-60">
             <label>PEP</label>
-			<input type="text" class="pep">				
+				<?php
+					@$id_campana_get = @$_REQUEST['campana'];
+					if($id_campana_get!='-1'){
+						@$registrosCampa=mysqli_query($conexion,"SELECT * from campana WHERE id_campana = '$id_campana_get'") or die ("Error en el select");
+						if(@$reg=mysqli_fetch_array($registrosCampa)){
+							@$id = $reg['id'];
+							echo "<span>$id</span>";
+						}
+					}else{
+						echo "";
+					}
+				?>
           </div>
 		  
           <div class="caja base-20">
             <label>N° Solicitud</label>
-			<input type="text" class="resizedTextbox">				
+			<input type="text" id="nro_solicitud" class="resizedTextbox">				
           </div>
 		  
            
 		  
            <div class="caja base-20">
             <label>Responsable</label>
-			<select name="campana" class="paraSelect">
-              <option selected="" value="-1"></option>
-            </select>		
+				<?php
+					if($reg=mysqli_fetch_array($registrosOC)){
+						$id_user = $reg['id_user'];
+						$jefe_autorizacion = $reg['jefe_autorizacion'];
+					}
+					
+					$registrosUser=mysqli_query($conexion,"SELECT * FROM members WHERE id = '$id_user'") or die ("Problemas en el select");
+						if($reg=mysqli_fetch_array($registrosUser)){
+							$nombre = $reg['nombre'];
+							$apellido = $reg['apellido'];
+						}
+						$nombre_completo = $nombre." ".$apellido;
+						
+					echo "<input type=\"text\" value=\"$nombre_completo\" class=\"resizedTextbox\">";
+				?>
           </div>
 		  
            <div class="caja base-60">
             <label>Autorizante</label>
-			<select name="campana" class="paraSelect">
-              <option selected="" value="-1"></option>
-            </select>		
+				<?php
+					echo "<input type=\"text\" value=\"$jefe_autorizacion\" class=\"resizedTextbox\">";
+				?>
           </div>
 		  
            <div class="caja base-20">
@@ -162,7 +256,7 @@ exit;
 			<!--aqui empezé-->
 		<!--aqui termine-->
         </form>
-        <form action="" class="registro-campana">
+        <form action="costos-accion-refresh.php" method="POST" class="registro-campana">
 		    <section class="grupo">
 		      <table class="table">
 		        <thead>
@@ -180,33 +274,76 @@ exit;
 		            <th>Nº OC</th>
 		          </tr>
 		        </thead>
-		        <tbody>
-		          <tr>
-		            <td class="area">Marketing Institucional</td>
-		            <td class="ceco">CEE1007752</td>
-		            <td class="desc-servicio">Impresos flyers</td>
-		            <td class="reg-gasto">Impresiones Mundo Experto Volantes/mailing directo/cartas/</td>
-		            <td class="pep">AM</td>
-		            <td class="control-presupuesto">Impresos</td>
-		            <td class="pep">IC</td>
-		            <td class="ppto-proyecto">$ 5.000.000</td>
-		            <td class="ppto-real">$ 7.000.000</td>
-		            <td class="diferencia">$ 2.000.000</td>
-		            <td class="nOC">123456789</td>
-		          </tr>
-		          <tr>
-		            <td class="area">Mundo Experto</td>
-		            <td class="ceco">CEE1007752</td>
-		            <td class="desc-servicio">Impresos flyers</td>
-		            <td class="reg-gasto">Impresiones </td>
-		            <td class="pep">AM</td>
-		            <td class="control-presupuesto">Impresos</td>
-		            <td class="pep">IC</td>
-		            <td class="ppto-proyecto">$ 155.000.000</td>
-		            <td class="ppto-real">$ 7.000.000</td>
-		            <td class="diferencia">$ 2.000.000</td>
-		            <td class="nOC">123456789</td>
-		          </tr>
+		        <tbody>		          
+					<?php
+						while($reg=mysqli_fetch_array($registrosOC2)){
+							
+							$numero_orden = $reg['numero_orden'];
+							$descripcion_oc = $reg['descripcion'];
+							$area_pago = $reg['area_pago'];
+							$registro_gasto = $reg['registro_gasto'];
+							$control_presupuesto = $reg['control_presupuesto'];
+							$total_final = $reg['total_final'];
+							
+							$total_sin_dots = str_replace(".","",$total_final);
+							$suma_PPTO = $suma_PPTO + $total_sin_dots;							
+							$suma_PPTO_format = number_format($suma_PPTO,0, ",", ".");
+							
+							$ppto_real_name = 'ppto';
+							$variable_increment = $variable_increment + 1;
+							$ppto_real_name_final = $ppto_real_name.$variable_increment;
+							
+							$nro_oc = 'nroOC';
+							$variable_increment2 = $variable_increment2 + 1;
+							$nro_oc_name_final = $nro_oc.$variable_increment2;
+							
+							$registrosAreaPago=mysqli_query($conexion,"SELECT * FROM centro_costo WHERE id_centro_costo = '$area_pago'") or die ("Problemas en el select");
+							
+							if($reg2=mysqli_fetch_array($registrosAreaPago)){
+								$descripcion = $reg2['descripcion'];
+								$ceco = $reg2['ceco'];
+							}
+							
+							$registrosRegistro=mysqli_query($conexion,"SELECT * FROM registro WHERE id_registro = $registro_gasto") or die ("Problemas en el select");
+							
+							if($reg3=mysqli_fetch_array($registrosRegistro)){
+								$registro_gasto_name = $reg3['registro_gasto'];
+								$id_sap_RG = $reg3['id'];
+							}
+							
+							$registrosControlPre=mysqli_query($conexion,"SELECT * FROM control_presupuesto WHERE id_controlP = $control_presupuesto") or die ("Problemas en el select");
+							
+							if($reg4=mysqli_fetch_array($registrosControlPre)){
+								$id_cp = $reg4['id'];
+								$control_pre_name = $reg4['control_presupuesto'];
+							}
+							
+							$registrosCac=mysqli_query($conexion,"SELECT * FROM cac WHERE nro_oc = $numero_orden") or die ("Problemas en el select");
+							
+							if($reg5=mysqli_fetch_array($registrosCac)){
+								$ppto_real = $reg5['ppto_real'];
+								$ppto_real_format = number_format($ppto_real,0, ",", ".");
+							}
+							
+							$ppto_real_suma = $ppto_real_suma + $ppto_real;
+							$ppto_real_suma_format = number_format($ppto_real_suma,0, ",", ".");
+							
+							echo "<tr>";
+							echo "<td class=\"area\">$descripcion</td>";
+							echo "<td class=\"ceco\">$ceco</td>";
+							echo "<td class=\"desc-servicio\">$descripcion_oc</td>";
+							echo "<td class=\"reg-gasto\">$registro_gasto_name</td>";
+							echo "<td class=\"pep\">$id_sap_RG</td>";
+							echo "<td class=\"control-presupuesto\">$control_pre_name</td>";
+							echo "<td class=\"pep\">$id_cp</td>";
+							echo "<td class=\"ppto-proyecto\">$ $total_final</td>";
+							echo "<td class=\"ppto-real\"><input type=\"text\" name=\"$ppto_real_name_final\" value=\"$ppto_real_format\" class=\"pptoReal\"></input></td>";
+							echo "<td class=\"diferencia\">$ppto_real_name_final</td>";
+							echo "<td class=\"nOC\"><input type=\"text\" name=\"$nro_oc_name_final\" value=\"$numero_orden\" class=\"ccs_nroOC\" readonly></input></td>";
+							echo "</tr>";
+						}
+						
+					?>
 		        </tbody>
 		      </table>
 		      <section class="grupo borde-tabla">
@@ -220,10 +357,25 @@ exit;
 		              <th> </th>
 		              <th> </th>
 		              <th>Total</th>
-		              <th class="tot-1">$ 99.000.000</th>
-		              <th class="tot-1">$ 5.000.000</th>
-		              <th class="tot-1">$ 5.000.000</th>
-		              <th> </th>
+						<?php
+							echo "<th class=\"tot-1\">$ $suma_PPTO_format</th>";
+							echo "<th class=\"tot-1\">$ $ppto_real_suma_format</th>";
+							echo "<th class=\"tot-1\">$ 5.000.000</th>";
+						?>
+		              <!--
+					  <th><a href="costos-accion.php">actualizar</a> </th>
+					  -->
+					  <?php
+						//echo "<th><a href=\"costos-accion.php?id_campana_send=",urlencode(@$id_campana_get2)," \">actualizar</a></th>";
+						echo "<input type=\"text\" value=\"$rows\" name=\"rows\" hidden=hidden />";
+						
+						echo "<input type=\"text\" value=\"Dummy\" name=\"ppto0\" hidden=hidden />";
+						echo "<input type=\"text\" value=\"Decoy\" name=\"nroOC0\" hidden=hidden />";
+						
+						echo "<input type=\"text\" id=\"nroSolicitudBox\" value=\"\" name=\"nro_solicitud_send\" />";
+						
+						echo "<th><button type=\"submit\">actualizar</button></th>";
+					  ?>
 		            </tr>
 		          </thead>
 		        </table>
